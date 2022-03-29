56248d9aa9ce0f52c6faeac60889ffc944fde813
ziadshimy7 <ziadshimy7@gmail.com>
2022-03-29 17:12:00 +0500

    added nodemailer auth

--- server/controllers/sendEmailController.js
+++ server/controllers/sendEmailController.js
@@ -1,19 +1,27 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config(); 
 export const sendEmail = async (req, res) => {
-  console.log(req.body);
   try {
     let transporter = nodemailer.createTransport({
-      service: "gmail",
+      service: "hotmail",
       auth: {
-        user: "ziadshimy77@gmail.com",
-        pass: "istudentziadshimy123!",
+        user: process.env.HOTMAIL_USERNAME,
+        pass: process.env.HOTMAIL_PASSWORD,
       },
+    });
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
     });
     let info = await transporter.sendMail({
       from: `${req.body.name} 👻 <${req.body.userEmail}>`,
       to: "ziadshimy7@gmail.com",
       subject: "Hello ✔",
-      text: "Hello world?",
+      text: "Hello world",
       html: `<b>${req.body.message}</b>`,
     });
     if (info) res.status(200).json({ message: "success" });
@@ -21,8 +29,5 @@ export const sendEmail = async (req, res) => {
     res
       .status(404)
       .json({ message: "Error sending email, Please try again later" });
-  }
-
-  if (info) {
   }
 };
--- server/controllers/worksController.js
+++ server/controllers/worksController.js
@@ -2,7 +2,6 @@ import Work from "../models/workModel.js";
 export const getWorks = async (req, res) => {
   try {
     const work = await Work.find();
-    console.log(work);
     if (work) res.status(200).json(work);
   } catch (error) {
     res.status(404).json({ message: "Fetching works failed" });
