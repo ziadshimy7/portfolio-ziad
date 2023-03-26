045fefb3dfe0733b869f262c1cd1d4c9f99174d6
Ziad <z.elshimy@kari.com>
2023-03-26 16:27:50 +0500

    feat: added sorting by year to experiences

--- server/controllers/experiencesController.js
+++ server/controllers/experiencesController.js
@@ -1,7 +1,8 @@
 import experiencesModel from "../models/experiencesModel.js";
 export const getExperiences = async (req, res) => {
   try {
-    const experiences = await experiencesModel.find();
+    const experiences = await experiencesModel.find().sort({ year: 1 });
+    console.log(experiences);
     res.status(200).json(experiences);
   } catch (error) {
     res.status(404).json({ message: "Not found" });
--- server/src/server.js
+++ server/src/server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/skills", skillsRouter);
 app.use("/experiences", experiencesRouter);
 app.use("/sendemail", sendEmailRouter);
